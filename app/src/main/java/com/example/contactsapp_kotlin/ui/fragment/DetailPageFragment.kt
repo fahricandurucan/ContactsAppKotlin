d1549b0abbd1f70db2b9f759d056749968ece0ae
fahricandurucan <fahricandurucan@icloud.com>
2023-05-01 18:17:10 +0300

    navigation and dataBinding added

--- app/src/main/java/com/example/contactsapp_kotlin/ui/fragment/DetailPageFragment.kt
+++ app/src/main/java/com/example/contactsapp_kotlin/ui/fragment/DetailPageFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.contactsapp_kotlin.R
+import com.example.contactsapp_kotlin.databinding.FragmentDetailPageBinding
 
 class DetailPageFragment : Fragment() {
+    private lateinit var binding: FragmentDetailPageBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_detail_page, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_detail_page, container, false)
+        return binding.root
     }
 
 }
