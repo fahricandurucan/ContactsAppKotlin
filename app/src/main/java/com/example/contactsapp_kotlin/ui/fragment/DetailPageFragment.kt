da8660c36c99a21ec6085bdd4314219bcf1e3fc4
fahricandurucan <fahricandurucan@icloud.com>
2023-05-01 17:20:23 +0300

    fragments created and basic design added

---
+++ app/src/main/java/com/example/contactsapp_kotlin/ui/fragment/DetailPageFragment.kt
@@ -0,0 +1,15 @@
+package com.example.contactsapp_kotlin.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.contactsapp_kotlin.R
+
+class DetailPageFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detail_page, container, false)
+    }
+
+}
